ae1f585419f777296773b3e0bdd18ec2a11b8b30
twer4774 <twer4774@gmail.com>
2019-09-23 14:07:07 +0900

    Init: Create Project & Configure Spring boot

---
+++ AdminPagePractice/src/main/java/com/walter/AdminPagePractice/AdminPagePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.walter.AdminPagePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPagePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPagePracticeApplication.class, args);
+	}
+
+}
